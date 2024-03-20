21268401a26bd4de52ff611bdf93fa648eeebe70
King-web254 <vwanyungu254@gmail.com>
2024-03-20 17:42:17 +0300

    active + notes

--- public/home.js
+++ public/home.js
@@ -15,15 +15,11 @@ schoolSelect.addEventListener('change', (e) => {
     if (schoolSelect.value == 'SCI') {
         departmentSelect.innerHTML = `
             <option value="Computer_Science" default>Computer Science</option>
-            <option value="Information Technology">Information Technology</option>
         ` 
-        console.log("CHANGED")
     } else if(schoolSelect.value == 'SOBE') {
         departmentSelect.innerHTML = `
-            <option value="Business Administration" default>Business Administration</option>
-            <option value="Economics">Economics</option>
+            
         ` 
-        console.log("CHANGED")
     }
 })
 
@@ -31,7 +27,12 @@ yearSelect.addEventListener('change', (e) => {
     if (yearSelect.value == 'Year_2.2' && schoolSelect.value == 'SCI' && departmentSelect.value == 'Computer_Science') {
         unitSelect.innerHTML = `
             <option value="Data_structures_and_algorithms" default>Data strcutures and algorithms</option>
-            <option value="Research methodology">Research methodology</option>
+            <option value="Research_methods">Research methods</option>
+            <option value="Client_server_computing">Client server computing</option>
+            <option value="Database_systems">Database systems</option>
+            <option value="Logic_programming">Logic programming</option>
+            <option value="Principles_of_OS">Principles of Operating Systems</option>
+            <option value="Web_programming">Web programming</option>
         ` 
     } 
 })
